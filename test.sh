336aa892ae39cecaa0e26573bae89d8b7c41e2b2
Henrik Giesel <hengiesel@gmail.com>
2019-06-06 18:00:41 +0200

    Improve test script
    skip ci

--- test.sh
+++ test.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
 if [[ "$1" ]]; then
-  TARGET=1
+  TARGET="$1"
 else
   if [[ ! "$TARGET" ]]; then
     TARGET='*'
   fi
 fi
 
+if [[ "$TARGET" == 'try' ]]; then
+  exec nvim -u 'tests/vimrc-test.vim'
+fi
 
-declare test_name="tests/${TARGET}-spec.vader"
+declare test_name="tests/${TARGET}*-spec.vader"
 
-nvim -Es -u 'tests/vimrc-test.vim' -c "Vader! ${test_name}"
+if [[ "$TARGET" == '*' ]]; then
+  nvim -Es -u 'tests/vimrc-test.vim' -c "Vader! ${test_name}" 2>&1 | egrep '^.*(Starting|Success).*$' | awk '1 ; (NR==1) || (NR>1 && NR%2==1) {printf"\n"}'
+
+else
+  nvim -Es -u 'tests/vimrc-test.vim' -c "Vader! ${test_name}" 2>&1
+fi
