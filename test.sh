0fc59a0be3eed381ebde34ded45f7f3803de38b8
Henrik Giesel <hengiesel@gmail.com>
2019-06-06 16:58:37 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-echo "$TARGET"
+tests_alias=(
+  'test/bracket/alias-spec.vader'
+)
 
 tests_field=(
   'test/bracket/field-short-if-spec.vader'
@@ -22,29 +24,13 @@ tests_expression=(
   'test/bracket/expression-multiline-indented-spec.vader'
 )
 
-for test_name in “${!TESTS_TO_EXECUTE}”
-  TESTS_TO_EXECUTE=TESTS_PART_$1[@]
-do
-  ./ui_test_run_script "$test_name.feature"
+tests_other=(
+  'test/other/indent-spec.vader'
+  'test/other/symbol-spec.vader'
+)
+
+TESTS_TO_EXECUTE="tests_$TARGET"
+
+for test_name in “${!TESTS_TO_EXECUTE}”; do
+  nvim -Es -u 'test/vimrc-test.vim' -c "Vader! $test_name"
 done
-
-# nvim -Es -u 'test/vimrc-test.vim' -c 'Vader! test/bracket/alias-spec.vader'
-
-# nvim -Es -u 'test/vimrc-test.vim' -c 'Vader! test/bracket/expression-general-spec.vader'
-# nvim -Es -u 'test/vimrc-test.vim' -c 'Vader! test/bracket/expression-whitespace-spec.vader'
-# nvim -Es -u 'test/vimrc-test.vim' -c 'Vader! test/bracket/expression-nested-spec.vader'
-# nvim -Es -u 'test/vimrc-test.vim' -c 'Vader! test/bracket/expression-multiline-trailing-spec.vader'
-# nvim -Es -u 'test/vimrc-test.vim' -c 'Vader! test/bracket/expression-multiline-leading-spec.vader'
-# nvim -Es -u 'test/vimrc-test.vim' -c 'Vader! test/bracket/expression-multiline-indented-spec.vader'
-
-# nvim -Es -u 'test/vimrc-test.vim' -c 'Vader! test/bracket/field-short-if-spec.vader'
-# nvim -Es -u 'test/vimrc-test.vim' -c 'Vader! test/bracket/field-short-af-spec.vader'
-# nvim -Es -u 'test/vimrc-test.vim' -c 'Vader! test/bracket/field-trailing-if-spec.vader'
-# nvim -Es -u 'test/vimrc-test.vim' -c 'Vader! test/bracket/field-trailing-af-spec.vader'
-# nvim -Es -u 'test/vimrc-test.vim' -c 'Vader! test/bracket/field-indented-if-spec.vader'
-# nvim -Es -u 'test/vimrc-test.vim' -c 'Vader! test/bracket/field-indented-af-spec.vader'
-# nvim -Es -u 'test/vimrc-test.vim' -c 'Vader! test/bracket/field-leading-if-spec.vader'
-# nvim -Es -u 'test/vimrc-test.vim' -c 'Vader! test/bracket/field-leading-af-spec.vader'
-
-# nvim -Es -u 'test/vimrc-test.vim' -c 'Vader! test/other/indent-spec.vader'
-# nvim -Es -u 'test/vimrc-test.vim' -c 'Vader! test/other/symbol-spec.vader'
